f1417fb9795eb14b5fc773c7d414b0468af5b9d7
mikseros <mmadynski@gmail.com>
2022-01-20 12:02:02 +0100

    Initial commit: new layout, index

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mikolaj Development</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body class="bg-gray-700 text-white">
+    <header class="fixed bg-gray-700 top-0 left-0 right-0 z-50">
+        <div class="container mx-auto flex justify-between p-4">
+            <h1 class="text-xl font-black">Mikolaj Development</h1>
+            <nav class="-mx-2">
+                <a href="{{ route('home') }}" class="text-lg mx-2 text-white hover:text-pink-500 transition">Home</a>
+                <a href="{{ route('about') }}" class="text-lg mx-2 text-white hover:text-pink-500 transition">About</a>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+        @yield('page-content')
+    </main>
+
+    <footer>
+        Footer
+    </footer>
+    
+</body>
+</html>
