7304cf287d0d16ab536ed516980e5708d97a4302
mikseros <mmadynski@gmail.com>
2022-01-20 12:19:19 +0100

    Some page content and footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.default')
 
 @section('page-content')
-    Home Page
+    <section class="relative min-h-screen flex items-center">
+        <div class="container mx-auto text-center">
+            <h2 class="text-4xl sm:text-8xl">Learn to <span class="text-pink-500">code</span></h2>
+            <h3 class="text-2xl sm:text-4xl italic">with Mikseros Development</h3>
+        </div>
+    </section>
 @endsection
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Mikolaj Development</title>
+    <title>Mikseros Development</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body class="bg-gray-700 text-white">
     <header class="fixed bg-gray-700 top-0 left-0 right-0 z-50">
         <div class="container mx-auto flex justify-between p-4">
-            <h1 class="text-xl font-black">Mikolaj Development</h1>
+            <h1 class="text-xl font-black">Mikseros Development</h1>
             <nav class="-mx-2">
                 <a href="{{ route('home') }}" class="text-lg mx-2 text-white hover:text-pink-500 transition">Home</a>
                 <a href="{{ route('about') }}" class="text-lg mx-2 text-white hover:text-pink-500 transition">About</a>
@@ -23,8 +23,10 @@
     </main>
 
     <footer>
-        Footer
+        <div class="container mx-auto p-4 content-center">
+            <p>&copy; Mikseros Development | Learn to code</p>
+        </div>
     </footer>
-    
+
 </body>
 </html>
